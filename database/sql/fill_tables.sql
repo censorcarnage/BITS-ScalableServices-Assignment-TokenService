5045040e09a9cc7b1b04f9a8b4a579885edc5984
censorcarnage <itskwadehra@gmail.com>
2022-11-16 22:55:24 +0530

    Changing Range

--- database/sql/fill_tables.sql
+++ database/sql/fill_tables.sql
@@ -1,19 +1,19 @@
-insert into range_central (rangeid,lower,higher,expired,used) values (1,1,100,0,0);
+insert into range_central (rangeid,lower,higher,expired,used) values (1,1001,2000,0,0);
 
-insert into range_central (rangeid,lower,higher,expired,used) values (2,101,200,0,0);
+insert into range_central (rangeid,lower,higher,expired,used) values (2,2001,3000,0,0);
 
-insert into range_central (rangeid,lower,higher,expired,used) values (3,201,300,0,0);
+insert into range_central (rangeid,lower,higher,expired,used) values (3,3001,4000,0,0);
 
-insert into range_central (rangeid,lower,higher,expired,used) values (4,301,400,0,0);
+insert into range_central (rangeid,lower,higher,expired,used) values (4,4001,5000,0,0);
 
-insert into range_central (rangeid,lower,higher,expired,used) values (5,401,500,0,0);
+insert into range_central (rangeid,lower,higher,expired,used) values (5,5001,6000,0,0);
 
-insert into range_central (rangeid,lower,higher,expired,used) values (6,501,600,0,0);
+insert into range_central (rangeid,lower,higher,expired,used) values (6,6001,7000,0,0);
 
-insert into range_central (rangeid,lower,higher,expired,used) values (7,601,700,0,0);
+insert into range_central (rangeid,lower,higher,expired,used) values (7,7001,8000,0,0);
 
-insert into range_central (rangeid,lower,higher,expired,used) values (8,701,800,0,0);
+insert into range_central (rangeid,lower,higher,expired,used) values (8,8001,9000,0,0);
 
-insert into range_central (rangeid,lower,higher,expired,used) values (9,801,900,0,0);
+insert into range_central (rangeid,lower,higher,expired,used) values (9,9001,10000,0,0);
 
-insert into range_central (rangeid,lower,higher,expired,used) values (10,901,1000,0,0);
+insert into range_central (rangeid,lower,higher,expired,used) values (10,10001,11000,0,0);
